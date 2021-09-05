74eacb1e974a6594ca2c0f98600880803a577489
berrysauce <contact@berrysauce.me>
2021-09-05 17:31:14 +0200

    Initial working commit

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import uvicorn
-from fastapi import FastAPI, Response, Request
+from fastapi import FastAPI, Response, Request, HTTPException, Form, status, Depends, Cookie
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from jose import JWTError, jwt
 from pydantic import BaseModel
 from typing import Optional
 from deta import Deta
 from dotenv import load_dotenv
 import os
 from tools import hashing
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 load_dotenv()
@@ -18,6 +21,41 @@ sitesdb = deta.Base("reacty-sites")
 templates = Jinja2Templates(directory="templates")
 
 app.mount("/assets", StaticFiles(directory="templates/assets"), name="assets")
+
+SECRET_KEY = str(os.getenv("AUTH_SECRET"))
+ALGORITHM = "HS256"
+# PRODUCTION EXPIRY: 720
+ACCESS_TOKEN_EXPIRE_MINUTES = 5
+
+def createtoken(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def get_current_user(access_token: Optional[str] = Cookie(None)):
+    if access_token is None:
+        raise HTTPException(status_code=401, detail="Unauthorized. Authentication failed.")
+    
+    try:
+        payload = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
+        key: str = payload.get("sub")
+        if key is None:
+            raise HTTPException(status_code=401, detail="Unauthorized. Authentication failed.")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Unauthorized. Authentication failed.")
+    site = sitesdb.get(key)
+    if site is False:
+        raise HTTPException(status_code=401, detail="Unauthorized. Authentication failed.")
+    return site["key"]
+
+class Site(BaseModel):
+    key: str
+
 class LoginSite(BaseModel):
     domain: str
     password: str
@@ -33,37 +71,76 @@ class LoginSite(BaseModel):
 def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+@app.get("/pricing")
+def root(request: Request):
+    return templates.TemplateResponse("pricing.html", {"request": request})
+
 @app.get("/login")
 def root(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
+@app.get("/signup")
+def root(request: Request):
+    return templates.TemplateResponse("signup.html", {"request": request})
+
 @app.get("/dashboard")
-def dashboard(request: Request):
-    site = sitesdb.fetch({"domain": "example.com"}).items[0]
+def dashboard(request: Request, key: str = Depends(get_current_user)):
+    site = sitesdb.get(key)
+        
     return templates.TemplateResponse("dashboard.html", {
         "request": request, 
         "domain": site["domain"], 
         "positive_reactions": site["feedback"]["positive"], 
         "neutral_reactions": site["feedback"]["neutral"], 
         "negative_reactions": site["feedback"]["negative"], 
-        "webkey": site["key"]
+        "webkey": key,
+        "widgettext": site["widget"]["text"]
     })
+    
 
 """
 -----------------------------------------------------------------------------
                               BACK END
 -----------------------------------------------------------------------------
 """
+@app.post("/login/auth")
+def loginauth(response: Response, username: str = Form(...), password: str = Form(...)):
+    domain = username
+    try:
+        site = sitesdb.fetch({"domain": domain}).items[0]
+    except IndexError:
+        raise HTTPException(status_code=401, detail="Unauthorized. Authentication failed.")
+    
+    if hashing.verifypw(site["password"], password) is False:
+        raise HTTPException(status_code=401, detail="Unauthorized. Authentication failed.")
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = createtoken(
+        data={"sub": site["key"]}, expires_delta=access_token_expires
+    )
+    
+    response = RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
+    response.set_cookie(key="access_token", value=access_token)
+    return response
+
+@app.get("/logout")
+def logout(response: Response, key: str = Depends(get_current_user)):
+    response = RedirectResponse(url="/login", status_code=status.HTTP_303_SEE_OTHER)
+    response.delete_cookie(key="access_token")
+    return response
 
 @app.post("/api/create")
-def create(site: LoginSite):
-    if len(sitesdb.fetch({"domain": site.domain}).items) != 0:
-        # ---------------------------------------------------------- CHANGE LATER
-        return {"msg": "Action failed, domain already exists!"}
-    sitesdb.insert({
-        "domain": site.domain,
-        "password": hashing.hashpw(site.password),
+def create(username: str = Form(...), password: str = Form(...)):
+    domain = username
+    if len(sitesdb.fetch({"domain": domain}).items) != 0:
+        raise HTTPException(status_code=409, detail="Domain is already registered.")
+    result = sitesdb.insert({
+        "domain": domain,
+        "password": hashing.hashpw(password),
         "created_on": str(datetime.now()),
+        "widget": {
+            "text": f"Any feedback for {domain}?"
+        },
         "feedback": {
             "positive": 0,
             "neutral": 0,
@@ -71,22 +148,33 @@ def create(site: LoginSite):
         },
         "locked": False
     })
-    # ---------------------------------------------------------- CHANGE LATER
-    return {"msg": "Action successfull!"}
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = createtoken(
+        data={"sub": result["key"]}, expires_delta=access_token_expires
+    )
+    
+    response = RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
+    response.set_cookie(key="access_token", value=access_token)
+    return response
 
 @app.get("/api/{key}/widget.js")
 def widgetjs(key: str):
     # add site key to widget code
+    site = sitesdb.get(key)
+    if site is False:
+        raise HTTPException(status_code=404, detail="Site key not found")
     with open("tools/widget.js", "r") as f:
         widget = str(f.read())
         widget = widget.replace("[webkey]", key)
+        widget = widget.replace("[widgettext]", site["widget"]["text"])
     return Response(content=widget, media_type="text/javascript")
 
 @app.get("/api/{key}/report/{feedback}")
 def getfeedback(key: str, feedback: str):
     site = sitesdb.get(key)
     if not site:
-        return {"msg": "Site key doesn't exist."}
+        raise HTTPException(status_code=404, detail="Site key not found")
     if feedback == "1":
         newamount = site["feedback"]["positive"] + 1
         sitesdb.update(key=key, updates={
@@ -114,6 +202,55 @@ def getfeedback(key: str, feedback: str):
                 "negative": newamount
             }
         })
+        
+@app.post("/api/settings")
+def widgetsettings(key: str = Depends(get_current_user), text: str = Form(...)):
+    site = sitesdb.get(key)
+    if site is False:
+        raise HTTPException(status_code=404, detail="Site key not found")
+    sitesdb.update(key=key, updates={
+        "widget": {
+            "text": text}
+    })
+    return RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
+
+@app.get("/api/clear")
+def clearwidget(key: str = Depends(get_current_user)):
+    site = sitesdb.get(key)
+    if site is False:
+        raise HTTPException(status_code=404, detail="Site key not found")
+    sitesdb.update(key=key, updates={
+        "feedback": {
+            "positive": 0,
+            "neutral": 0,
+            "negative": 0
+        }
+    })
+    return RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
+
+@app.get("/api/delete")
+def deletewidget(key: str = Depends(get_current_user)):
+    site = sitesdb.get(key)
+    if site is False:
+        raise HTTPException(status_code=404, detail="Site key not found")
+    sitesdb.delete(key)
+    return RedirectResponse(url="/login", status_code=status.HTTP_303_SEE_OTHER)
+        
+        
+"""
+-----------------------------------------------------------------------------
+                            EXCEPTION HANDLER
+-----------------------------------------------------------------------------
+"""
+
+@app.exception_handler(StarletteHTTPException)
+async def custom_exception_handler(request: Request, exc: StarletteHTTPException):
+    if exc.status_code == 404:
+        return templates.TemplateResponse("error.html", {"request": request, "code": "404", "detail": "Sorry, but the requested resource couldn't be found. Try again later or check your request."})
+    else:
+        return templates.TemplateResponse("error.html", {"request": request, "code": str(exc.status_code), "detail": exc.detail})
+
+
 
 
 if __name__ == "__main__":
--- tools/widget-beta.js
+++ tools/widget-beta.js
@@ -1,52 +1,34 @@
 document.write(`
-    <div role="alert" id="reactyWidget" style="border-radius: 10px;border-style: none;font-family: Inter, sans-serif;opacity: 1;background: rgba(0,0,0,0.85);padding: 10px;position: fixed;bottom: 0;left: 0;margin-left: 20px;margin-right: 20px;margin-bottom: 20px;opacity: 1;-webkit-transition: opacity 500ms linear;transition: opacity 500ms linear;">
-    <div role="alert" style="padding: 12px;padding-bottom: 0px;">
-        <div style="text-align: center; margin-bottom: 15px;">
-            <strong style="color: rgb(255,255,255);font-size: 20px;line-height: 26px;font-family: Inter, sans-serif;">What do you think?<br /></strong>
-            <button type="button" class="reaction" style="background: rgba(255,255,255,0);border-style: none;font-size: 35px;margin-top: 10px" onclick="returnFeedback(1)">
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-mood-happy" style="font-size: 35px;color: rgb(74,217,24);">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <circle cx="12" cy="12" r="9"></circle>
-                    <line x1="9" y1="9" x2="9.01" y2="9"></line>
-                    <line x1="15" y1="9" x2="15.01" y2="9"></line>
-                    <path d="M8 13a4 4 0 1 0 8 0m0 0h-8"></path>
-                </svg>
-            </button>
-            <button type="button" class="reaction" style="background: rgba(255,255,255,0);border-style: none;font-size: 35px;margin-top: 10px" onclick="returnFeedback(2)">
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-mood-neutral" style="font-size: 35px;color: rgb(224,228,34);">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <circle cx="12" cy="12" r="9"></circle>
-                    <line x1="9" y1="10" x2="9.01" y2="10"></line>
-                    <line x1="15" y1="10" x2="15.01" y2="10"></line>
-                    <line x1="9" y1="15" x2="15" y2="15"></line>
-                </svg>
-            </button>
-            <button type="button" class="reaction" style="background: rgba(255,255,255,0);border-style: none;font-size: 35px;margin-top: 10px" onclick="returnFeedback(3)">
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-mood-sad" style="font-size: 34px;color: rgb(231,38,38);">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <circle cx="12" cy="12" r="9"></circle>
-                    <line x1="9" y1="10" x2="9.01" y2="10"></line>
-                    <line x1="15" y1="10" x2="15.01" y2="10"></line>
-                    <path d="M9.5 15.25a3.5 3.5 0 0 1 5 0"></path>
-                </svg>
-            </button>
+    <div role="alert" id="reactyWidget" style="border-radius: 10px;border-style: none;font-family: Inter, sans-serif;opacity: 1;background: rgba(0,0,0,0.85);padding: 10px;position: fixed;bottom: 0;left: 0;margin-left: 20px;margin-right: 20px;margin-bottom: 20px;z-index: 99;opacity: 1;-webkit-transition: opacity 1000ms linear;transition: opacity 1000ms linear;">
+        <div role="alert" style="padding: 12px;padding-bottom: 0px;">
+            <div style="text-align: center; margin-bottom: 15px;">
+                <strong id="widgetTitle" style="color: rgb(255,255,255);font-size: 20px;line-height: 26px;font-family: Inter, sans-serif;">What do you think?<br /></strong>
+                <button id="widgetPositive" type="button" class="reaction" style="background: rgba(255,255,255,0);border-style: none;font-size: 35px;margin-top: 15px" onclick="returnFeedback(1)">😃</button>
+                <button id="widgetNeutral" type="button" class="reaction" style="background: rgba(255,255,255,0);border-style: none;font-size: 35px;margin-top: 15px" onclick="returnFeedback(2)">😐</button>
+                <button id="widgetNegative" type="button" class="reaction" style="background: rgba(255,255,255,0);border-style: none;font-size: 35px;margin-top: 15px" onclick="returnFeedback(3)">🙁</button>
+            </div>
+            <p style="margin-bottom: 6px;color: rgb(130,130,130);font-size: 12px;margin-top: 9px;font-family: Inter, sans-serif;">powered by <a style="text-decoration: underline; color: rgb(130,130,130);" href="https://reacty.net?ref=widget" target="_blank">reacty</a>
+                <button type="button" style="font-size: 12px;padding: 0px;background: rgba(0,0,0,0);border-style: none;font-family: Inter, sans-serif; color: rgba(255,255,255); float: right;" onclick="closeWidget()">Close</button>
+            </p>
         </div>
-        <p style="margin-bottom: 6px;color: rgb(130,130,130);font-size: 12px;margin-top: 9px;font-family: Inter, sans-serif;">powered by <a style="text-decoration: underline; color: rgb(130,130,130);" href="https://reacty.net?ref=widget" target="_blank">reacty</a>
-            <button type="button" style="font-size: 12px;padding: 0px;background: rgba(0,0,0,0);border-style: none;font-family: Inter, sans-serif; color: rgba(255,255,255); float: right;" onclick="closeWidget()">Close</button>
-        </p>
-    </div>
     </div>
 
     <script>
-    function closeWidget() {
-        const widget = document.getElementById("reactyWidget")
-        widget.style.opacity = '0';
-        setTimeout(function(){widget.style.visibility = "hidden";}, 500);
-    }
-    function returnFeedback(feedback) {
-        let url = "https://reacty-testing.deta.dev/api/[webkey]/report/" + feedback
-        fetch(url)
-        closeWidget()
-    }
+        function closeWidget() {
+            const widget = document.getElementById("reactyWidget")
+            widget.style.opacity = '0';
+            setTimeout(function(){widget.style.visibility = "hidden";}, 1000);
+        }
+        function returnFeedback(feedback) {
+            let url = "https://reacty.net/api/[webkey]/report/" + feedback
+            fetch(url)
+
+            document.getElementById("widgetPositive").style.visibility = "hidden";
+            document.getElementById("widgetNeutral").style.visibility = "hidden";
+            document.getElementById("widgetNegative").style.visibility = "hidden";
+            document.getElementById("widgetTitle").innerHTML = "Thank you!";
+
+            closeWidget()
+        }
     </script>
 `)
